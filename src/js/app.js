d46ee9e221c3fa42818ae94e38b047b830d1937b
Olga <blacksmith@skillpoint.ru>
2022-07-20 15:03:19 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/function.js";
+flsFunctions.isWebp();
+
+
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper('.box', {
+
+});
